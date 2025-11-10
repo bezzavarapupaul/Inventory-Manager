aacb6e7276b3e8fe5b25954be37a5e6e4ffde9be
bezzavarapupaul <bezzavarapupaulbabu@gmail.com>
2025-11-10 13:21:16 +0530

    db update cred

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,9 @@
 const { Pool } = require("pg");
 
+
 const pool = new Pool({
-  user: "postgres",       
-  host: "localhost",
-  database: "authdb",
-  password: "admin",  
-  port: 5432,
+  connectionString: "postgresql://authdb_7u9a_user:E7eRHBlbWmCDos07IMDeMgx1cBh7X4MA@dpg-d48pekemcj7s73e3lg00-a/authdb_7u9a",
+  ssl: { rejectUnauthorized: false }  
 });
 
 module.exports = pool;
